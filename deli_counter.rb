834267364bc9f47c1b88bf6774629ea63203151e
Sienna <remicknsisi@gmail.com>
2023-03-09 12:30:14 -0500

    commit tree a18a798453607178291e5ea90c93d203c27e8789

--- deli_counter.rb
+++ deli_counter.rb
@@ -1,1 +1,13 @@
-# Write your code here.
+katz_deli = []
+
+def line katz_deli
+    i = katz_deli.length
+    if i > 0
+        while i > 0 
+            puts "The line is currently: #{i}. #{katz_deli[i]}"
+            i -= 1
+        end
+    else
+        puts "The line is currently empty."
+    end
+end
